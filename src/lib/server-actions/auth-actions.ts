ef9b2bf59509dbde05ee1b34350e3dbe61c6ec7c
Ankit Patel <131870832+Ank0it@users.noreply.github.com>
2026-02-21 17:47:07 +0530

    Refactor authentication actions to use fetch API for login and signup

--- src/lib/server-actions/auth-actions.ts
+++ src/lib/server-actions/auth-actions.ts
@@ -1,39 +1,30 @@
-'use server';
+'use client';
 
 import { z } from 'zod';
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { FormSchema } from '../types';
-import { cookies } from 'next/headers';
 
 export async function actionLoginUser({
   email,
   password,
 }: z.infer<typeof FormSchema>) {
-  const supabase = createRouteHandlerClient({ cookies });
-  const response = await supabase.auth.signInWithPassword({
-    email,
-    password,
+  const res = await fetch('/api/auth/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+    body: JSON.stringify({ email, password }),
   });
-  return response;
+  return res.json();
 }
 
 export async function actionSignUpUser({
   email,
   password,
 }: z.infer<typeof FormSchema>) {
-  const supabase = createRouteHandlerClient({ cookies });
-  const { data } = await supabase
-    .from('profiles')
-    .select('*')
-    .eq('email', email);
-
-  if (data?.length) return { error: { message: 'User already exists', data } };
-  const response = await supabase.auth.signUp({
-    email,
-    password,
-    options: {
-      emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}api/auth/callback`,
-    },
+  const res = await fetch('/api/auth/signup', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+    body: JSON.stringify({ email, password }),
   });
-  return response;
+  return res.json();
 }
